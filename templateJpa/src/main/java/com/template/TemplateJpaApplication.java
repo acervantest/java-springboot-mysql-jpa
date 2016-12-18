776c4b5d83612fbdcac2af9aa10de1315c67dce2
alejandro <alejandro@alejandros-Air.hitronhub.home>
2016-12-18 15:38:41 -0800

    configuration: basic config

---
+++ templateJpa/src/main/java/com/template/TemplateJpaApplication.java
@@ -0,0 +1,12 @@
+package com.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateJpaApplication.class, args);
+	}
+}
